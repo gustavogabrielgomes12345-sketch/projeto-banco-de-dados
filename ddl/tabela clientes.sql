e2ff80da2ab6eefd23245e6a6f935f04cf781826
PedroHenriquereis19 <pedrohenhique52@gmail.com>
2026-09-08 19:55:19 -0300

    Add files via upload

---
+++ ddl/tabela clientes.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE clientes (
+    cliente_id      INT PRIMARY KEY,
+    nome            VARCHAR(150) NOT NULL,
+    cpf_cnpj        VARCHAR(20)  NOT NULL,
+    telefone        VARCHAR(20),
+    email           VARCHAR(150),
+    endereco        VARCHAR(200),
+    data_cadastro   DATE NOT NULL,
+    ativo           BOOLEAN NOT NULL DEFAULT TRUE
+);
