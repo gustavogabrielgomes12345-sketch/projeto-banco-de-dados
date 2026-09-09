af68840f532218f22677d62ee7acfa65f84de169
gustavogabrielgomes12345-sketch <gustavogabrielgomes12345@gmail.com>
2026-09-09 19:24:01 -0300

    atualizar

--- ddl/tabela tipos funcionario.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE tipos_funcionario (
-    tipo_funcionario_id INT PRIMARY KEY,
-    descricao            VARCHAR(50) NOT NULL
-);
--- ddl/tabela_tipos_funcionario.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE tipos_funcionario (
-    tipo_funcionario_id INT PRIMARY KEY,
-    descricao            VARCHAR(50) NOT NULL,
-    CONSTRAINT uq_tipos_funcionario_descricao UNIQUE (descricao)
-);
