b9a44abb1acd48fa7aac6746cbcaf3aea65ff3f3
gustavogabrielgomes12345-sketch <gustavogabrielgomes12345@gmail.com>
2026-09-09 18:27:25 -0300

    Delete ddl/tabela fornecedores.sql

--- ddl/tabela fornecedores.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE fornecedores (
-    fornecedor_id   INT PRIMARY KEY,
-    razao_social    VARCHAR(150) NOT NULL,
-    cnpj            VARCHAR(20) NOT NULL,
-    telefone        VARCHAR(20),
-    email           VARCHAR(150),
-    endereco        VARCHAR(200)
-);
