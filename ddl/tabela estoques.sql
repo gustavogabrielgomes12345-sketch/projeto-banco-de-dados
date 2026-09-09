90e6a4b7ede5ebae4dcbfcd0eee71d4ecf6f745b
gustavogabrielgomes12345-sketch <gustavogabrielgomes12345@gmail.com>
2026-09-09 18:27:05 -0300

    Delete ddl/tabela estoques.sql

--- ddl/tabela estoques.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE estoques (
-    estoque_id          INT PRIMARY KEY,
-    peca_id             INT NOT NULL,
-    quantidade_atual    INT NOT NULL DEFAULT 0,
-    quantidade_minima   INT NOT NULL DEFAULT 0,
-    localizacao         VARCHAR(50),
-    data_atualizacao    DATE
-);
