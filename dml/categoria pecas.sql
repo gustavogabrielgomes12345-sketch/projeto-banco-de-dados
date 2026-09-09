faad6814c707d80104232fd2bd95aa88af5554b3
PedroHenriquereis19 <pedrohenhique52@gmail.com>
2026-09-09 19:46:36 -0300

    Add files via upload

---
+++ dml/categoria pecas.sql
@@ -0,0 +1,9 @@
+INSERT INTO categorias_peca (categoria_peca_id, descricao) VALUES
+(1, 'Motor'),
+(2, 'Freios'),
+(3, 'Suspensão'),
+(4, 'Elétrica'),
+(5, 'Filtros e Fluidos'),
+(6, 'Arrefecimento'),
+(7, 'Transmissão'),
+(8, 'Carroceria');
