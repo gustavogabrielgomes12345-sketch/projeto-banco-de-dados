320c9aaaa5e5cd7b1bcfb2d13cb22c3f60b462e3
gustavogabrielgomes12345-sketch <gustavogabrielgomes12345@gmail.com>
2026-09-09 18:27:46 -0300

    Delete ddl/tabela ordens servico servicos.sql

--- ddl/tabela ordens servico servicos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE ordens_servico_servicos (
-    ordem_servico_servico_id   INT PRIMARY KEY,
-    ordem_servico_id           INT NOT NULL,
-    servico_id                 INT NOT NULL,
-    funcionario_id             INT NOT NULL, 
-    valor_cobrado               DECIMAL(10,2) NOT NULL
-);
