73f888f52048dd412988d85c8418f1b3f7e1d6b1
PedroHenriquereis19 <pedrohenhique52@gmail.com>
2026-09-09 19:45:19 -0300

    Add files via upload

---
+++ dml/fornecedores.sql
@@ -0,0 +1,5 @@
+INSERT INTO fornecedores (fornecedor_id, razao_social, cnpj, telefone, email, endereco) VALUES
+(1, 'AutoPeças Bahia Ltda', '22.333.444/0001-55', '(71) 3200-1000', 'vendas@autopecasbahia.com.br', 'Av. Industrial, 500'),
+(2, 'Distribuidora Motor Sul', '33.444.555/0001-66', '(71) 3200-2000', 'comercial@motorsul.com.br', 'Rua das Oficinas, 300'),
+(3, 'Peças & Cia Nordeste', '44.555.666/0001-77', '(71) 3200-3000', 'contato@pecasecia.com.br', 'Rod. BA-093, km 12'),
+(4, 'Grupo AutoPart Brasil', '55.666.777/0001-88', '(71) 3200-4000', 'vendas@autopartbrasil.com.br', 'Av. das Indústrias, 1200');
